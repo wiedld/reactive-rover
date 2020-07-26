9d0c55a824f49f63c57433a02e5438c1821bcfac
wiedld <dlw405@gmail.com>
2020-07-25 22:41:38 -0700

    webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,16 +1,45 @@
-module: {
-    rules: [
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+      oop: './src/oop/app.ts',
+      functional: './src/functional/app.ts'
+    },
+    output: {
+      path: path.resolve(__dirname, 'lib'),
+      filename: '[name].js'
+    },
+    devServer: {
+      contentBase: './lib',
+      hot: true
+    },
+    devtool: 'cheap-module-eval-source-map',
+    module: {
+      rules: [
         {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env'],
+              presets: [
+                '@babel/preset-env',
+                "@babel/preset-react",
+                '@babel/preset-typescript'
+              ],
               plugins: ['@babel/plugin-proposal-object-rest-spread']
             }
           }
-        }
-    ]
+        },
+        { test: /\.css$/, use: 'css-loader' },
+        { test: /\.ts$/, use: 'ts-loader' }
+      ]
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new HtmlWebpackPlugin({template: './src/index.html'})
+    ],
+    mode: 'development'
   }
-  
