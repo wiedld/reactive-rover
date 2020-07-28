c6b50ec34d2b8a37770f64d5877b9955ae09b361
wiedld <dlw405@gmail.com>
2020-07-28 13:11:39 -0700

    add in robot class, and corresponding test cases

---
+++ client/src/robot/index.ts
@@ -0,0 +1,410 @@
+const STATUS_CODES = ['OK', 'OBSTACLE', 'INVALID_COMMAND'];
+
+
+enum Terrian {P = 'Plains', M = 'Mountains', C = 'Crevasse'};
+type Command = 'L' | 'R' | 'F' | 'B';
+type Direction = 'N' | 'S' | 'E' | 'W';
+type Location = [number,number];
+type World = Array<Array<Terrian>>;
+
+class PhysicalWorld {
+    // top left corner is (X:0, Y:0)
+    // bottom right is (X:4, Y:4)
+    static WORLD: World = [
+        [Terrian.P, Terrian.P, Terrian.P, Terrian.C, Terrian.P],
+        [Terrian.P, Terrian.M, Terrian.P, Terrian.C, Terrian.P],
+        [Terrian.P, Terrian.M, Terrian.P, Terrian.C, Terrian.P],
+        [Terrian.P, Terrian.M, Terrian.P, Terrian.P, Terrian.P],
+        [Terrian.P, Terrian.M, Terrian.P, Terrian.P, Terrian.P]
+    ];
+
+    static TERRAIN_TYPES = {
+        [Terrian.P]: {
+          obstacle: false,
+          description: 'plains'
+        },
+        [Terrian.M]: {
+          obstacle: true,
+          description: 'mountains'
+        },
+        [Terrian.C]: {
+          obstacle: true,
+          description: 'crevasse'
+        }
+      };
+
+    static getTerrain ([x,y]: Location) {
+        return PhysicalWorld.WORLD[y] && PhysicalWorld.WORLD[y][x];
+    }
+
+    static isOffWorld ([x,y]: Location) {
+        return x < 0 || y < 0 || y >= PhysicalWorld.WORLD.length || x >= PhysicalWorld.WORLD[0].length;
+    }
+
+    static isSameLoc (loc1: Location, loc2: Location) {
+        return loc1[0] == loc2[0] && loc1[1] == loc2[1];
+    }
+
+    _objects: Array<Array<number>>;
+
+    removeLocation ([x,y]: Location) {
+        !(PhysicalWorld.isOffWorld([x,y])) && this._objects[y][x]--;
+    }
+
+    setLocation ([x,y]: Location) {
+        !(PhysicalWorld.isOffWorld([x,y])) && this._objects[y][x]++;
+    }
+
+    constructor () {
+        this._objects = Array(5).fill(0)
+            .map((_) => Array(5).fill(0))
+    }
+
+    isObstacle (targetLoc: Location, currLoc: Location) {
+        const [x,y] = targetLoc;
+        const terrain = PhysicalWorld.getTerrain([x,y]);
+        const numOtherObjectsAtLoc = this._objects[y][x] - (PhysicalWorld.isSameLoc(currLoc, [x,y]) ? 1 : 0);
+
+        return PhysicalWorld.TERRAIN_TYPES[terrain].obstacle || 0 != numOtherObjectsAtLoc;
+    }
+}
+
+class Robot {
+    static ObstacleException = 1;
+    static InvalidCmdException = 2;
+    // ordered for mod math (rotating robot)
+    static orderedDir: Array<Direction> = [
+        'N',
+        'E',
+        'S',
+        'W'
+    ];
+
+    static isKnownWorld ([x,y]: Location, knownWorld: World) {
+        return !!knownWorld[y] && !!knownWorld[y][x];
+    }
+
+    _location: Location;
+    _direction: number;
+    _commands: Array<Command>;
+    _physicalWorld: PhysicalWorld;
+
+    setLocation (loc: Location) {
+        this._physicalWorld.removeLocation(this._location);
+        this._physicalWorld.setLocation(loc);
+        this._location = loc;
+    }
+    get location () { return this._location; }
+
+    setDirection (dir: number) { this._direction = dir; }
+    get direction () { return Robot.orderedDir[this._direction]; }
+
+    addCommand (c: Command) { this._commands.push(c); }
+    addCommands (cs: Array<Command>) { this._commands.push(...cs); }
+    get commands () { return this._commands; }
+
+    constructor (physicalWorld: PhysicalWorld, location: Location, direction: Direction) {
+        this._commands = [];
+        this._physicalWorld = physicalWorld;
+        this._physicalWorld.setLocation(location);
+        this._location = location;
+        this._direction = Robot.orderedDir.indexOf(direction);
+    }
+
+    command (commands: Array<Command>) {
+        try {
+            commands.forEach(cmd => this._move(cmd));
+            return this._status(0);
+        } catch (e) {
+            return this._status(e);
+        }
+    }
+
+    _move (cmd: Command) {
+        this.addCommand(cmd);
+
+        const mvtAlongXaxis = (cmd: Command) =>
+            (cmd == 'F' && this.direction == 'W') || (cmd == 'B' && this.direction == 'E')
+                ? - 1
+                : + 1;
+        
+        const mvtAlongYaxis = (cmd: Command) =>
+            (cmd == 'F' && this.direction == 'N') || (cmd == 'B' && this.direction == 'S')
+                ? - 1
+                : 1;
+
+        switch (cmd) {
+            case 'L':
+                this.setDirection(this._direction == 0 ? 3 : this._direction - 1);
+                break;
+            case 'R':
+                this.setDirection((this._direction + 1) % 4);
+                break;
+            case 'F':
+            case 'B':
+                let [x,y] = this.location;
+                if (['W', 'E'].includes(this.direction)) {
+                    x = x + mvtAlongXaxis(cmd);
+                } else {
+                    y = y + mvtAlongYaxis(cmd);
+                }
+                this.setLocation([x,y]);
+                break;
+            default:
+                throw Robot.InvalidCmdException;
+        }
+        this._assessOutcome(cmd);
+    }
+
+    _assessOutcome (cmd: Command) {
+        const reverseCmd: {[cmd: string]: Command } = {
+            'L': 'R',
+            'R': 'L',
+            'F': 'B',
+            'B': 'F'
+        };
+        const loc = this.location;
+        if (PhysicalWorld.isOffWorld(loc) || this._physicalWorld.isObstacle(loc, loc)) {
+            // Note: could input a special error code into commands (history)
+            this._move(reverseCmd[cmd]);
+            throw Robot.ObstacleException;
+        }
+    }
+
+    _status (status: number) {
+        return {
+            dir: this.direction,
+            loc: [...this.location],
+            status: STATUS_CODES[status]
+        };
+    }
+
+    /*
+        Use cases:
+            - pre-planning:
+                - when already aware of the terrian map.
+                    --> nextTileOkay() can lookahead at the terrian.
+                - plan optimal path first. then move.
+            - physical exploration:
+                - when unaware of terrian.
+                    --> nextTileOkay() only knows the edge of the world.
+                - note: physical exploration will backtrack over known terrian
+                    --> recursive call to the pre-planning path.
+    */
+    moveTo (location: Location, prePlan = false, knownWorld = PhysicalWorld.WORLD) {
+        try {
+            if (prePlan) {
+                this.command(this._buildPlan(location, knownWorld))
+            } else
+                this._physicalExploration(location);
+
+            return this._status(0);
+        } catch (e) {
+            return this._status(e);
+        }     
+    }
+
+    /*
+        Takes:
+            - goalLoc [x,y]
+            - knownWorld (known terrian map)
+        Returns:
+            - an array of directions [N, W, W, S, S]
+    */
+    _buildPlan (goalLoc: Location, knownWorld: World): Array<Command> {
+        const nextTileIsOkay = (loc: Location) => Robot.isKnownWorld(loc, knownWorld)
+            && !PhysicalWorld.isOffWorld(loc)
+            && !this._physicalWorld.isObstacle(loc, this.location);
+
+        const planningOnly = true;
+
+        return this._aStar(goalLoc, nextTileIsOkay, planningOnly);
+    }
+
+    /*
+        Takes:
+            - goalLoc [x,y]
+        Returns:
+            - true
+    */
+    _physicalExploration (goalLoc: Location) {
+        // only know size of world.
+        const nextTileIsOkay = (loc: Location) => !PhysicalWorld.isOffWorld(loc);
+
+        return this._aStar(goalLoc, nextTileIsOkay, false);
+    }
+
+    /*
+        Implement version of A*.
+            - common algorithmic function used by planning & exploration.
+        Takes:
+            - `goal` target location.
+            - `planningOnly` bool, determines if rover moves after each planning step.
+            - `nextTileOkay` function, restricts what parts of the world can be planned.
+        Returns:
+            - an array of directions [N, W, W, S, S], or bool true.
+    */
+    _aStar (goal: Location, nextTileIsOkay: (x: Location) => boolean, planningOnly: boolean) {
+        // Array<Array<typeof visitedNode>>
+        const visited: Array<Array<typeof visitedNode>> = [...Array(PhysicalWorld.WORLD.length)]
+            .map(_ => Array(PhysicalWorld.WORLD[0].length));
+
+        const toVisit: {[cost: number]: Array<Location> } = {}; // will be keyed by [estimatedCostToGoal]
+
+        const notYetVisited = ([x,y]: Location) => visited[y][x] == undefined;
+        const estimateBtwnLoc = ([x,y]: Location, [x1,y1]: Location) => Math.abs(x1-x) + Math.abs(y1-y);
+        
+        // start node
+        let loc: Location = this.location;
+        let [x,y] = loc;
+        let cost = estimateBtwnLoc([x,y], goal);
+
+        // visited
+        const visitedNode: { cost: number, path: Array<Command>, dir: Direction }
+            = { cost, path: [], dir: this.direction };
+        visited[y][x] = visitedNode;
+
+        // toVisit
+        toVisit[cost] = [loc];
+
+        let curr: typeof visitedNode;
+        let currDir: Direction;
+
+        while (estimateBtwnLoc(loc, goal) !== 0) {
+            curr = visited[y][x];
+            currDir = planningOnly ? visited[y][x].dir : this.direction;
+
+            const makeNext = (locN: Location, dir: Direction) => {
+                if (nextTileIsOkay(locN)) {
+                        let [x,y] = locN;
+
+                        if (notYetVisited(locN)) {
+                            const cost = estimateBtwnLoc(locN, goal);
+                            const { dir: resultDir, cmds } = this._convertDirToCmd([dir], currDir);
+
+                            visited[y][x] = { cost, dir: resultDir, path: curr.path.concat(cmds) };
+                            toVisit[cost] = toVisit.hasOwnProperty(cost)
+                                ? toVisit[cost].concat([locN])
+                                : [locN];
+                        }
+                }
+            };
+            // calc N
+            makeNext([x,y-1], 'N');
+            // calc S
+            makeNext([x,y+1], 'S');
+            // calc W
+            makeNext([x-1,y], 'W');
+            // calc E
+            makeNext([x+1,y], 'E');
+
+            // pick next move based on:
+            //      - edge-cost = est cost to move (e.g. backtracking)
+            //      - g-cost = already traveled node cost = path length on next node.
+            //      - h-cost = next node cost = est distance to goal (of next node). i.e. benefit gained.
+            const calcCost = (nodeLoc: Location, currLoc: Location): number => {
+                // geo distances
+                const estimateBtwnLoc = ([x,y]: Location, [x1,y1]: Location) => Math.abs(x1-x) + Math.abs(y1-y);
+                const nodeToGoal = estimateBtwnLoc(nodeLoc, goal);
+                const currToNode = estimateBtwnLoc(currLoc, nodeLoc);
+                const startToNode = visited[y][x].path.length;
+
+                // weights
+                const e = 1;
+                const g = planningOnly ? 1 : 0;
+                const h = 1;
+
+                return e * currToNode + g * startToNode + h * nodeToGoal;
+            };
+
+            const pickMin = (): Location | null => {
+                const options = Object.keys(toVisit);
+                if (options.length == 0) return null;
+
+                const toFind: { minCost: number, toVisitKey: number | null, nextLoc: Location | null }
+                     = { minCost: Infinity, toVisitKey: null, nextLoc: null };
+
+                const { toVisitKey, nextLoc } = options.sort((a,b) => a < b ? -1 : 1)
+                    .reduce((acc, cost) => {
+                        let nodeToGoal = parseInt(cost);
+                        toVisit[nodeToGoal].forEach(locN => {
+                            let [x,y] = locN;
+                            const totalCost = calcCost(locN, loc);
+                            if (acc.minCost > totalCost) {
+                                acc.minCost = totalCost;
+                                acc.nextLoc = locN;
+                                acc.toVisitKey = nodeToGoal;
+                            }
+                        });
+                        return acc;
+                    }, toFind);
+                
+                if (!toVisitKey || !nextLoc) return null;
+
+                // remove node from toVisit
+                toVisit[toVisitKey] = toVisit[toVisitKey].filter(l => l !== nextLoc);
+                if (toVisit[toVisitKey].length == 0)
+                    delete toVisit[toVisitKey];
+
+                return nextLoc;
+            };
+            let next = pickMin();
+
+            // no solution
+            if (!next)
+                throw Robot.InvalidCmdException;
+            else
+                loc = next;
+
+            // if planning only, do not move Robot.
+            if (!planningOnly) {
+                // @ts-ignore
+                this.moveTo(loc, true, visited);
+                loc = this.location;
+            }
+
+            x = loc[0];
+            y = loc[1];
+        }
+        return planningOnly ? visited[y][x].path : [];
+    }
+
+    /*  Takes:
+            - an array of directions [N,W,E,S,E,E]
+            - a starting direction
+        Returns:
+            - an object: {
+                cmd: array of rover commands,
+                dir: final direction   
+            }
+    */
+    _convertDirToCmd (directions: Array<Direction>, startDir = this.direction) {
+        const cmds: Array<Command> = [];
+
+        return [directions.reduce(({dir, ...acc}, d) => {
+            const goal: number = Robot.orderedDir.indexOf(d);
+            const cmds: Array<Command> = [];
+
+            // rotate
+            while (dir !== goal) {
+                if (dir < goal) {
+                    cmds.push('R');
+                    dir++;
+                } else {
+                    cmds.push('L');
+                    dir--;
+                }
+            }
+            // advance
+            cmds.push('F');
+
+            return {
+                dir,
+                cmds: acc.cmds.concat(cmds)
+            };
+        }, { dir: Robot.orderedDir.indexOf(startDir), cmds })]
+        .map(x => ({ ...x, dir: Robot.orderedDir[x.dir] }))[0];
+    }
+}
+
+export { PhysicalWorld, Robot };
---
+++ client/tests/robot.ts
@@ -0,0 +1,240 @@
+// @ts-nocheck
+import mocha from "mocha";
+import { expect } from "chai";
+import { PhysicalWorld, Robot } from "../src/robot/index";
+
+describe('Mars Robot', function() {
+    let rover1 = null, rover2 = null, rover3 = null;
+    let world;
+    beforeEach(function() {
+      world = new PhysicalWorld();
+      rover1 = new Robot(world, [2, 2], 'N');
+    });
+
+    describe('When the Mars Robot is initialized', function() {
+      it('should set the starting location', function() {
+        expect(rover1.location).to.deep.equal([2, 2]);
+      });
+      it('should set the starting direction', function() {
+        expect(rover1.direction).to.equal('N');
+      });
+    });
+    describe('When the rover recieves commands', function() {
+      it('should store the commands', function() {
+        rover1.command(['F', 'F', 'B']);
+        expect(rover1.commands).to.deep.equal(['F', 'F', 'B']);
+      });
+      it('should handle invalid commands', function() {
+        const status = rover1.command(['X']);
+
+        expect(status).to.deep.equal({
+          status: 'INVALID_COMMAND',
+          loc: [2, 2],
+          dir: 'N'
+        });
+      });
+    });
+
+    describe('When the rover executes valid commands', function() {
+      describe('When facing north', function() {
+        describe('When moving forward', function() {
+          it('should move north one tile', function() {
+            const status = rover1.command(['F']);
+            expect(status).to.deep.equal({
+              status: 'OK',
+              loc: [2, 1],
+              dir: 'N'
+            });
+          });
+        });
+        describe('When moving backward', function() {
+          it('should move south one tile', function() {
+            const status = rover1.command(['B']);
+            expect(status).to.deep.equal({
+              status: 'OK',
+              loc: [2, 3],
+              dir: 'N'
+            });
+          });
+        });
+        describe('When turning left', function() {
+          it('should be facing west', function() {
+            const status = rover1.command(['L']);
+            expect(status).to.deep.equal({
+              status: 'OK',
+              loc: [2, 2],
+              dir: 'W'
+            });
+          });
+        });
+        describe('When turning right', function() {
+          it('should be facing east', function() {
+            const status = rover1.command(['R']);
+            expect(status).to.deep.equal({
+              status: 'OK',
+              loc: [2, 2],
+              dir: 'E'
+            });
+          });
+        });
+      });
+    });
+
+    describe('When the rover encounters obstacles', function() {
+      describe('When encountering a mountain', function() {
+        it('should stop and return status', function() {
+          const status = rover1.command(['L', 'F']);
+          expect(status).to.deep.equal({
+            status: 'OBSTACLE',
+            loc: [2, 2],
+            dir: 'W'
+          });
+        });
+      });
+      describe('When encountering a crevasse', function() {
+        it('should stop and return status', function() {
+          const status = rover1.command(['F', 'F', 'R', 'F']);
+          expect(status).to.deep.equal({
+            status: 'OBSTACLE',
+            loc: [2, 0],
+            dir: 'E'
+          });
+        });
+      })
+      describe('When encountering the edge of the world', function() {
+        it('should stop and return status', function() {
+          const status = rover1.command(['F', 'F', 'F']);
+          expect(status).to.deep.equal({
+            status: 'OBSTACLE',
+            loc: [2, 0],
+            dir: 'N'
+          });
+        });
+      });
+    });
+
+    describe("A* moveTo algo, with pre-planning", function () {
+        it("move to NW corner", function () {
+            const loc = [0,0];
+            const status = rover1.moveTo(loc, true);
+            // console.log("# commands:", rover1.commands.length);
+            expect(status.status).to.equal('OK');
+            expect(status.loc).to.deep.equal(loc);
+        });
+        it("move to SE corner", function () {
+            const loc = [4,4];
+            const status = rover1.moveTo(loc, true);
+            // console.log("# commands:", rover1.commands.length);
+            expect(status.status).to.equal('OK');
+            expect(status.loc).to.deep.equal(loc);
+        });
+        it("move to SW corner", function () {
+            const loc = [0,4];
+            const status = rover1.moveTo(loc, true);
+            // console.log("# commands:", rover1.commands.length);
+            expect(status.status).to.equal('OK');
+            expect(status.loc).to.deep.equal(loc);
+        });
+        it("move to NE corner", function () {
+            const loc = [4,0];
+            const status = rover1.moveTo(loc, true);
+            // console.log("# commands:", rover1.commands.length);
+            expect(status.status).to.equal('OK');
+            expect(status.loc).to.deep.equal(loc);
+        });
+        it("should not move", function () {
+            const loc = [2,2];
+            const status = rover1.moveTo(loc, true);
+            expect(status.status).to.equal('OK');
+            // console.log("# commands:", rover1.commands.length);
+            expect(status.loc).to.deep.equal(loc);
+            expect(rover1.commands.length).to.equal(0);
+        });
+        it("error when not possible", function () {
+            const status = rover1.moveTo([1,1], true);
+            expect(status.status).to.equal('INVALID_COMMAND');
+        });
+    });
+
+    describe("A* moveTo algo, WITHOUT pre-planning", function () {
+        it("move to NW corner", function () {
+            const loc = [0,0];
+            const status = rover1.moveTo(loc);
+            // console.log("# commands:", rover1.commands.length);
+            expect(status.status).to.equal('OK');
+            expect(status.loc).to.deep.equal(loc);
+        });
+        it("move to SE corner", function () {
+            const loc = [4,4];
+            const status = rover1.moveTo(loc);
+            // console.log("# commands:", rover1.commands.length);
+            expect(status.status).to.equal('OK');
+            expect(status.loc).to.deep.equal(loc);
+        });
+        it("move to SW corner", function () {
+            const loc = [0,4];
+            const status = rover1.moveTo(loc);
+            // console.log("# commands:", rover1.commands.length);
+            expect(status.status).to.equal('OK');
+            expect(status.loc).to.deep.equal(loc);
+        });
+        it("move to NE corner", function () {
+            const loc = [4,0];
+            const status = rover1.moveTo(loc);
+            // console.log("# commands:", rover1.commands.length);
+            expect(status.status).to.equal('OK');
+            expect(status.loc).to.deep.equal(loc);
+        });
+        it("should not move", function () {
+            const loc = [2,2];
+            const status = rover1.moveTo(loc);
+            // console.log("# commands:", rover1.commands.length);
+            expect(status.status).to.equal('OK');
+            expect(status.loc).to.deep.equal(loc);
+            expect(rover1.commands.length).to.equal(0);
+        });
+        it("error when not possible", function () {
+            const status = rover1.moveTo([1,1]);
+            expect(status.status).to.equal('INVALID_COMMAND');
+        });
+    });
+
+    describe("Will handle multiple Robots in the world", () => {
+        beforeEach(function() {
+          world = new PhysicalWorld();
+          rover1 = new Robot(world, [2, 2], 'N');
+        });
+        describe("should not error if does not encounter another rover", () => {
+          it("workz", () => {
+            rover1.moveTo([4,4]);
+
+            rover2 = new Robot(world, [2, 2], 'N');
+            const status = rover2.command(['F', 'F', 'B']);
+            expect(status.status).to.equal('OK');
+          });
+      });
+        describe("if encountering another rover", () => {
+            it("during comand() sequence, then perform OBSTACLE error", () => {
+                let cmds = ['F', 'F', 'B'];
+                const status1 = rover1.command(cmds);
+                expect(status1.status).to.equal('OK');
+
+                rover2 = new Robot(world, [2, 2], 'N');
+                const status2 = rover2.command(cmds.concat(['L', 'L']));
+                expect(status2.status).to.equal('OBSTACLE');
+            });
+
+            it("during moveTo() planning, then perform INVALID_COMMAND error", () => {
+                let loc = [4,4]
+                const status1 = rover1.moveTo(loc);
+                expect(status1.loc).to.deep.equal(loc);
+                expect(status1.status).to.equal('OK');
+
+
+                rover3 = new Robot(world, [2, 2], 'N');
+                const status2 = rover3.moveTo(loc);
+                expect(status2.status).to.equal('INVALID_COMMAND');
+            });
+        });
+    });
+});
