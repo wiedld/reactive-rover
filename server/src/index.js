00019274eee786c37cc23677d1df3bdd6ae1d9a3
wiedld <dlw405@gmail.com>
2020-08-02 15:43:17 -0700

    cleanup server

--- server/src/index.js
+++ server/src/index.js
@@ -26,35 +26,3 @@ app.use(cookieSession({
 app.disable('x-powered-by'); // Helmet does this by default
 
 app.listen(3000, () => console.log("Example app listening"));
-
-/*
-  TODO:
-  - review network course
-  - review SDI interview
-*/
-
-/*
-    Use csurf middleware to protect against cross-site request forgery (CSRF).
-
-    Always filter and sanitize user input to protect against cross-site scripting (XSS) and command injection attacks.
-
-    Defend against SQL injection attacks by using parameterized queries or prepared statements.
-
-    Use the open-source sqlmap tool to detect SQL injection vulnerabilities in your app.
-
-    Use the nmap and sslyze tools to test the configuration of your SSL ciphers, keys, and renegotiation as well as the validity of your certificate.
-
-    Use safe-regex to ensure your regular expressions are not susceptible to regular expression denial of service attacks.
-*/
-/*
-Helmet is actually just a collection of smaller middleware functions that set security-related HTTP response headers:
-    - csp sets the Content-Security-Policy header to help prevent cross-site scripting attacks and other cross-site injections.
-    - hidePoweredBy removes the X-Powered-By header.
-    hpkp Adds Public Key Pinning headers to prevent man-in-the-middle attacks with forged certificates.
-    - hsts sets Strict-Transport-Security header that enforces secure (HTTP over SSL/TLS) connections to the server.
-    - ieNoOpen sets X-Download-Options for IE8+.
-    - noCache sets Cache-Control and Pragma headers to disable client-side caching.
-    - noSniff sets X-Content-Type-Options to prevent browsers from MIME-sniffing a response away from the declared content-type.
-    - frameguard sets the X-Frame-Options header to provide clickjacking protection.
-    - xssFilter sets X-XSS-Protection to enable the Cross-site scripting (XSS) filter in most recent web browsers.
-*/
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,9 +3,6 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello World"));
 
-app.get("/req", (req, res) => res.send(JSON.stringify(Object.keys(req))));
-app.get("/res", (req, res) => res.send(JSON.stringify(Object.keys(res))));
-
 app.post('/', function (req, res) {
     res.send('Got a POST request')
   });
